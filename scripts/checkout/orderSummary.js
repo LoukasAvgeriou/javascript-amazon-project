77d6748d2a8e36e4168822fde2a97db5432daf5a
Loukas <loukasavgeriou@gmail.com>
2025-01-04 22:15:14 +0200

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import { cart, removeFromCart, updateDeliveryOption } from '../../data/cart.js';
-import { products } from '../../data/products.js';
-import { formatCurrrency } from '../utils/money.js';
+import { products, getProduct } from '../../data/products.js';
+import { formatCurrency } from '../utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions } from '../../data/deliveryOption.js';
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOption.js';
 
 
 
@@ -16,38 +16,26 @@ console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function renderOrderSummary() {
 
-    let cartSummaryHTML = '';
+  let cartSummaryHTML = '';
 
-    cart.forEach((cartItem) => {
-        const productId = cartItem.productId;
+  cart.forEach((cartItem) => {
+    const productId = cartItem.productId;
 
-        let matchingProduct;
+    const matchingProduct = getProduct(productId);
 
-        products.forEach((product) => {
-            if (product.id === productId) {
-                matchingProduct = product;
-            }
-        });
+    const deliveryOptionId = cartItem.deliveryOptionId;
 
-        const deliveryOptionId = cartItem.deliveryOptionId;
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-        let deliveryOption;
+    const today = dayjs();
+    const deliveryDate = today.add(
+      deliveryOption.deliveryDays,
+      'days'
+    );
+    const dateString = deliveryDate.format('dddd, MMMM D');
 
-        deliveryOptions.forEach((option) => {
-            if (option.id === deliveryOptionId) {
-                deliveryOption = option;
-            }
-        });
-
-        const today = dayjs();
-        const deliveryDate = today.add(
-            deliveryOption.deliveryDays,
-            'days'
-        );
-        const dateString = deliveryDate.format('dddd, MMMM D');
-
-        cartSummaryHTML +=
-            `
+    cartSummaryHTML +=
+      `
     <div class="cart-item-container 
     js-cart-item-container-${matchingProduct.id}">
           <div class="delivery-date">
@@ -62,7 +50,7 @@ export function renderOrderSummary() {
                 ${matchingProduct.name}
               </div>
               <div class="product-price">
-                $${formatCurrrency(matchingProduct.priceCents)}
+                $${formatCurrency(matchingProduct.priceCents)}
               </div>
               <div class="product-quantity">
                 <span>
@@ -87,25 +75,25 @@ export function renderOrderSummary() {
         </div>
 
     `;
-    });
+  });
 
-    function deliveryOptionsHTML(matchingProduct, cartItem) {
+  function deliveryOptionsHTML(matchingProduct, cartItem) {
 
-        let html = '';
+    let html = '';
 
-        deliveryOptions.forEach((deliveryOption) => {
-            const today = dayjs();
-            const deliveryDate = today.add(
-                deliveryOption.deliveryDays,
-                'days'
-            );
-            const dateString = deliveryDate.format('dddd, MMMM D');
+    deliveryOptions.forEach((deliveryOption) => {
+      const today = dayjs();
+      const deliveryDate = today.add(
+        deliveryOption.deliveryDays,
+        'days'
+      );
+      const dateString = deliveryDate.format('dddd, MMMM D');
 
-            const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrrency(deliveryOption.priceCents)} -`;
+      const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} -`;
 
-            const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+      const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
-            html += `
+      html += `
               <div class="delivery-option js-delivery-option"
                 data-product-id = "${matchingProduct.id}"
                 data-delivery-option-id = "${deliveryOption.id}">
@@ -120,33 +108,33 @@ export function renderOrderSummary() {
                 </div>
               </div>
     `;
-        });
-
-        return html;
-    }
-
-    document.querySelector('.js-order-summary')
-        .innerHTML = cartSummaryHTML;
-
-    document.querySelectorAll('.js-delete-link')
-        .forEach((link) => {
-            link.addEventListener('click', () => {
-                const productId = link.dataset.productId
-                removeFromCart(productId);
-
-                const container = document.querySelector(`.js-cart-item-container-${productId}`);
-
-                container.remove();
-            })
-        });
-
-    document.querySelectorAll('.js-delivery-option').forEach((element) => {
-        element.addEventListener('click', () => {
-            const { productId, deliveryOptionId } = element.dataset;
-            updateDeliveryOption(productId, deliveryOptionId);
-            renderOrderSummary();
-        })
     });
+
+    return html;
+  }
+
+  document.querySelector('.js-order-summary')
+    .innerHTML = cartSummaryHTML;
+
+  document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+      link.addEventListener('click', () => {
+        const productId = link.dataset.productId
+        removeFromCart(productId);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+        container.remove();
+      })
+    });
+
+  document.querySelectorAll('.js-delivery-option').forEach((element) => {
+    element.addEventListener('click', () => {
+      const { productId, deliveryOptionId } = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+      renderOrderSummary();
+    })
+  });
 
 }
 
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,5 +1,5 @@
-export function formatCurrrency(priceCents) {
-    return (priceCents / 100).toFixed(2);
+export function formatCurrency(priceCents) {
+    return (Math.round(priceCents) / 100).toFixed(2);
 }
 
-export default formatCurrrency;
+export default formatCurrency;
